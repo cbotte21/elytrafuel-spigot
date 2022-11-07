dafb3186d4799d288835d14c8cc8f5543629dd7e
Samkist <samshark23451@gmail.com>
2022-11-06 23:27:21 -0500

    fixed firework detonating

--- src/main/java/me/cbotte21/elytrafuel/events/ElytraBoostEvent.java
+++ src/main/java/me/cbotte21/elytrafuel/events/ElytraBoostEvent.java
@@ -1,11 +1,15 @@
 package me.cbotte21.elytrafuel.events;
 
 import com.destroystokyo.paper.event.player.PlayerElytraBoostEvent;
+import java.util.Optional;
 import me.cbotte21.elytrafuel.battery.BatteryPayloadType;
 import net.kyori.adventure.text.Component;
+import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
@@ -20,6 +24,22 @@ public class ElytraBoostEvent implements Listener {
         this.namespaces = namespaces;
         this.prefix = prefix;
         this.wearNotification = wearNotification;
+    }
+
+    @EventHandler
+    public void onInteract(PlayerInteractEvent event) {
+        Optional<ItemStack> item = Optional.ofNullable(event.getItem());
+        if(item.isPresent()) {
+            Material type = item.get().getType();
+            if(type.equals(Material.FIREWORK_ROCKET) && Objects.nonNull(event.getClickedBlock())) {
+                ItemMeta fireworkMetadata = event.getItem().getItemMeta();
+                namespaces.forEach(namespace -> {
+                    if(fireworkMetadata.getPersistentDataContainer().has(namespace)) {
+                        event.setCancelled(true);
+                    }
+                });
+            }
+        }
     }
 
     @EventHandler
